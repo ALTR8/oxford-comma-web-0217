bcaa812d88f5440f797f523e5bb63210fa49812b
Alexandra Roth <alroth8@gmail.com>
2017-01-10 19:54:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
 
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length == 1
+    return array.join()
+  elsif array.length >= 2
+    last = array.pop
+    array.join(", ") + ", and " + last.to_s
+  end
 end
